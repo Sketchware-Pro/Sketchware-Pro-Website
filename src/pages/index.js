ae8ac608dbfe72fabfc92160e4c7898b1a07e041
nicesapien-too <79096599+nicesapien-too@users.noreply.github.com>
2022-07-16 19:04:41 +0530

    Nice title

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
+      title="Home"
       description="Sketchware Pro's official website">
       <HomepageHeader />
       <main>
