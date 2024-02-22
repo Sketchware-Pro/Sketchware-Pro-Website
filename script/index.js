0f2cbe16ab2109d11b3e6e82a316f92d074fde74
NiceSapien <71333407+NiceSapien@users.noreply.github.com>
2024-02-22 20:31:00 +0530

    Updated for iOS

--- script/index.js
+++ script/index.js
@@ -2,3 +2,7 @@ function openNav() {
     document.getElementsByClassName("nav-links[0]").style.width = "70%";
     document.getElementById("main").style.display = "block";
 }
+if (navigator.userAgent.indexOf('iPhone') > -1 || navigator.userAgent.indexOf('iPad') > -1) {
+    document.getElementsByClassName('rocket[0]').style.display = 'none';
+    document.querySelector('.fallback-image').style.display = 'block';
+  }
