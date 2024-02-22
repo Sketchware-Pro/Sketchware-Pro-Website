ab85b70f59e1dee56ce7bf83c2e09160fb960c04
NiceSapien <71333407+NiceSapien@users.noreply.github.com>
2024-02-22 20:45:39 +0530

    Static PNG for iPhones and iPads

--- script/index.js
+++ script/index.js
@@ -2,7 +2,13 @@ function openNav() {
     document.getElementsByClassName("nav-links[0]").style.width = "70%";
     document.getElementById("main").style.display = "block";
 }
-if (navigator.userAgent.indexOf('iPhone') > -1 || navigator.userAgent.indexOf('iPad') > -1) {
-    document.getElementsByClassName('rocket[0]').style.display = 'none';
-    document.querySelector('.fallback-image').style.display = 'block';
-  }
+window.addEventListener('DOMContentLoaded', function() {
+    const rocket = document.querySelector('.rocket'); // Use querySelector
+  
+    if (navigator.userAgent.indexOf('iPhone') > -1 || navigator.userAgent.indexOf('iPad') > -1) {
+      rocket.style.display = 'none'; // Access style directly after selection
+      document.querySelector('.fallback-image').style.display = 'block';
+    } else {
+      console.log('Not an iPhone or iPad');
+    }
+  });
