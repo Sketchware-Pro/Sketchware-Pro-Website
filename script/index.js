df876f259628d46eb3dbfce12f25ea1364f11c93
NiceSapien <71333407+NiceSapien@users.noreply.github.com>
2024-02-25 18:07:34 +0530

    New sections: Comparison and features, animation, some mobile optimizations

--- script/index.js
+++ script/index.js
@@ -12,3 +12,21 @@ window.addEventListener('DOMContentLoaded', function() {
       console.log('Not an iPhone or iPad');
     }
   });
+
+ /* function reveal() {
+    var reveals = document.querySelectorAll("#main2");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal(); */
